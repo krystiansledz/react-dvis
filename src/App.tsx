af61856587c9967bf549b23a09cde1377c086d82
krystiansledz <krystiansledz419@gmail.com>
2023-01-03 20:35:16 +0100

    Change router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import { BrowserRouter } from "react-router-dom";
-import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterLuxon } from "@mui/x-date-pickers/AdapterLuxon";
-import { Routes, Route } from "react-router";
+import { HashRouter } from 'react-router-dom';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterLuxon } from '@mui/x-date-pickers/AdapterLuxon';
+import { Routes, Route } from 'react-router';
 import Generator from './Generator';
 
 function App() {
   const theme = createTheme({});
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <LocalizationProvider dateAdapter={AdapterLuxon}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
-            <Route path={"/*"} element={<Generator/>} />
+            <Route path={'*'} element={<Generator />} />
           </Routes>
         </ThemeProvider>
       </LocalizationProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
