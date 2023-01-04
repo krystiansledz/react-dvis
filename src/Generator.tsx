e4e751ca51675a7f4edfd75bd4240c5eb31717e4
krystiansledz <krystiansledz419@gmail.com>
2023-01-04 21:31:08 +0100

    .

--- src/Generator.tsx
+++ src/Generator.tsx
@@ -2,15 +2,10 @@ import { ChartGenerator } from 'react-dvis-test';
 import React from 'react';
 import fisData from './files/fis-data.json';
 import redmineConfig from './files/config.json';
-import { useLocation } from 'react-router-dom';
 
 const Generator = () => {
-  const location = useLocation();
-  console.log(location);
-
   return (
     <>
-      {location.pathname}
       <ChartGenerator data={fisData.issues} config={redmineConfig} basePath='/package' />
     </>
   );
