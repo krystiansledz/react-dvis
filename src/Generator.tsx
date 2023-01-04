c65af976593ea449f20dc9b44af6aa35ce5385b2
krystiansledz <krystiansledz419@gmail.com>
2023-01-04 21:13:45 +0100

    Fix router

--- src/Generator.tsx
+++ src/Generator.tsx
@@ -2,8 +2,12 @@ import { ChartGenerator } from 'react-dvis-test';
 import React from 'react';
 import fisData from './files/fis-data.json';
 import redmineConfig from './files/config.json';
+import { useLocation } from 'react-router-dom';
 
 const Generator = () => {
+  const location = useLocation();
+  console.log(location);
+
   return (
     <>
       <ChartGenerator data={fisData.issues} config={redmineConfig} />
